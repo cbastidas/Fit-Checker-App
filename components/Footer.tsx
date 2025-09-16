22c30fc185143b09964d2fba81470b20930aaae6
Christian <christian@leapsquare.com>
2025-09-16 15:58:31 +0200

    Fixing the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,12 +35,12 @@ const Footer: React.FC<FooterProps> = ({ isOnDressingScreen = false }) => {
         <p>
           Created by{' '}
           <a 
-            href="https://x.com/ammaar" 
+            href="https://github.com/cbastidas" 
             target="_blank" 
             rel="noopener noreferrer"
             className="font-semibold text-gray-800 hover:underline"
           >
-            @ammaar
+            Chris Bastidas
           </a>
         </p>
         <div className="h-4 mt-1 sm:mt-0 flex items-center overflow-hidden">
